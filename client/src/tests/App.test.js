f0b8e77d256165729cf2a113f928ae8805c7b9ab
BeijiYang <langyuyang@outlook.com>
2019-05-18 14:47:09 +0200

    add: App test

--- client/src/tests/App.test.js
+++ client/src/tests/App.test.js
@@ -1,11 +1,11 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
 import { configure, mount, shallow } from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
 import configureMockStore from 'redux-mock-store'
 import thunk from 'redux-thunk'
 import App from '../App'
+import items from '../components/items'
 
 configure({ adapter: new Adapter() })
 const middlewares = [thunk]
@@ -13,17 +13,37 @@ const mockStore = configureMockStore(middlewares)
 const store = mockStore({ itemsToShow: [] })
 
 describe('App', () => {
-  it('renders without crashing', () => {
-    const wraper = shallow(
+  it('renders 2 routes, home and frameWorks', () => {
+    const wraper = mount(
       <Provider store={store}>
         <App />
       </Provider>
     )
+    expect(wraper.find('[href="/"]')).toHaveLength(1)
+    expect(wraper.find('[href="/frameWorks"]')).toHaveLength(1)
   })
-  it('renders 2 routes', () => { })
 
-  it('home page renders HomeTitle', () => { })
+  it('home page renders HomeTitle', () => {
+    const homeTitle = 'Home'
+    const wraper = mount(
+      <Provider store={store}>
+        <App />
+      </Provider>
+    )
+    expect(wraper.contains(homeTitle)).toBe(true)
+  })
 
-  it('frameWorks page renders input and cards', () => { })
+  it('frameWorks page renders input and cards', () => {
+    const wraper = mount(
+      <Provider store={store}>
+        <App />
+      </Provider>
+    )
+    // click link
+    wraper.find('[href="/frameWorks"]').simulate('click', { button: 0 })
+    // render input and cards
+    expect(wraper.find('input')).toHaveLength(1)
+    expect(wraper.find('Card')).toHaveLength(items.length)
+  })
 })
 
