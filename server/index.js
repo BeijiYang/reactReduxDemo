f95b5097319a6e899807f3839e9f250212fb1415
BeijiYang <langyuyang@outlook.com>
2019-05-19 15:31:46 +0200

    add: a simple demo server

---
+++ server/index.js
@@ -0,0 +1,39 @@
+const express = require('express')
+const cors = require('cors')
+const bodyParser = require('body-parser')
+
+const app = express()
+
+const TOTAL_PAGE_NUM = 10
+const getIntSmallerThan = num => Math.floor(Math.random() * num)
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+app.use(cors())
+
+app.get('/', (req, res) => {
+  res.send('hello')
+})
+
+app.post('/companies', (req, res) => {
+  const { body: { pageIndex, cardsNumPerPage } } = req
+
+  const getData = length => Array.from({ length }, () => ({
+    id: Math.random(),
+    name: `Company No.${getIntSmallerThan(1000)}`,
+    email: 'email@email.com',
+    phone: '0123456789',
+    intro: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+    url: `https://picsum.photos/300?random=${getIntSmallerThan(1000)}`,
+  }))
+
+  const companies = getData(cardsNumPerPage)
+  res.send({
+    companies,
+    totalPageNum: TOTAL_PAGE_NUM
+  })
+})
+
+app.listen(3001, () => {
+  console.log('Your server is running on port 3001')
+})
