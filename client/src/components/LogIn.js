3e58f7d5b56b8e0d19471fa5b7519f803c5fa12b
BeijiYang <langyuyang@outlook.com>
2019-05-21 01:01:17 +0200

    add: style for login page

--- client/src/components/LogIn.js
+++ client/src/components/LogIn.js
@@ -4,34 +4,35 @@ import { connect } from 'react-redux'
 import { login } from '../actions/auth'
 import Button from '../components/CustomButton'
 import '../styles/login.scss'
-
+import '../styles/form.scss'
+// todo: 抽出个纯展示的 presentional component
 const LogIn = ({ login }) => {
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
 
   return (<div className="login">
     <div className="form">
-      <div className="from-line">
-        <span>username</span>
+      <div className="form-line">
+        <div className="form-key">username</div>
         <input
           type="text"
           onChange={({ target: { value } }) => setUsername(value)}
         />
       </div>
-      <div className="from-line">
-        <span>password</span>
+      <div className="form-line">
+        <div className="form-key">password</div>
         <input
-          type="number"
+          type="password"
           onChange={({ target: { value } }) => setPassword(value)}
         />
       </div>
-    </div>
-    <Button
-      color="primary"
-      onClick={() => login(username, password)}
-    >
-      login
+      <Button
+        color="primary"
+        onClick={() => login(username, password)}
+      >
+        login
       </Button>
+    </div>
   </div>)
 }
 
