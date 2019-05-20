5e4df8197f89da1b1d2979bda5a2c7690067c627
BeijiYang <langyuyang@outlook.com>
2019-05-21 00:25:55 +0200

    add: form using React Hooks

--- client/src/components/LogIn.js
+++ client/src/components/LogIn.js
@@ -1,20 +1,39 @@
-import React from 'react'
+import React, { useState } from 'react'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 import { login } from '../actions/auth'
 import Button from '../components/CustomButton'
 import '../styles/login.scss'
 
-const LogIn = ({ login }) => (
-  <div className="login">
+const LogIn = ({ login }) => {
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+
+  return (<div className="login">
+    <div className="form">
+      <div className="from-line">
+        <span>username</span>
+        <input
+          type="text"
+          onChange={({ target: { value } }) => setUsername(value)}
+        />
+      </div>
+      <div className="from-line">
+        <span>password</span>
+        <input
+          type="number"
+          onChange={({ target: { value } }) => setPassword(value)}
+        />
+      </div>
+    </div>
     <Button
       color="primary"
-      onClick={login}
+      onClick={() => login(username, password)}
     >
       login
       </Button>
-  </div>
-)
+  </div>)
+}
 
 LogIn.propTypes = {
   login: PropTypes.func.isRequired,
