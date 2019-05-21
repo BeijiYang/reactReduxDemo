1720a4ea88b6bcc347ccb8b300c6911a82976029
BeijiYang <langyuyang@outlook.com>
2019-05-21 17:54:42 +0200

    refactor: login

--- client/src/components/Router.js
+++ client/src/components/Router.js
@@ -1,8 +1,8 @@
 import React from 'react'
-import { Switch, Route, Redirect } from 'react-router-dom'
+import { Switch, Route } from 'react-router-dom'
 import { connect } from 'react-redux'
 import Home from './Home'
-import LogIn from './LogIn'
+import LogIn from '../containers/LogIn'
 import NotFound from './NotFound'
 import FrameWorks from '../containers/FrameWorks'
 
