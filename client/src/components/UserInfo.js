353a1fded97740a5b4ce5808a42a43ee072458cb
BeijiYang <langyuyang@outlook.com>
2019-05-21 18:19:34 +0200

    refactor: UserInfo

--- client/src/components/UserInfo.js
+++ client/src/components/UserInfo.js
@@ -1,11 +1,17 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
+import '../styles/userInfo.scss'
 
 function UserInfo({ isAuthenticated, username }) {
   return (
-    <div>
-      {isAuthenticated ? `Welcome ${username}` : 'Please Login'}
+    <div className="user-info">
+      {isAuthenticated
+        ? <div>
+          <div className="greeting">{`Welcome ${username}`}</div>
+          <div className="account">Your account: $6969</div>
+        </div>
+        : <div className="greeting" >Please Login</div >}
     </div>
   )
 }
