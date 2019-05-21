a78f223bc0b4b63d028c53b841f8d81acdf3be82
BeijiYang <langyuyang@outlook.com>
2019-05-21 21:48:48 +0200

    add: CustomRoute go back when authed

--- client/src/components/Router.js
+++ client/src/components/Router.js
@@ -3,25 +3,31 @@ import { Switch, Route } from 'react-router-dom'
 import { connect } from 'react-redux'
 import Home from './Home'
 import LogIn from '../containers/LogIn'
+import SignUp from '../containers/SignUp'
 import NotFound from './NotFound'
 import FrameWorks from '../containers/FrameWorks'
+
+// go back when authed
+const CustomRoute = ({ authed, path, component }) => (
+  <Route
+    path={path}
+    render={props => {
+      if (authed) {
+        props.history.goBack()
+      } else {
+        return component
+      }
+    }}
+  />
+)
 
 const Router = ({ isAuthenticated }) => {
   return (
     <Switch>
       <Route exact path="/" component={Home} />
       <Route path="/frameWorks" component={FrameWorks} />
-      <Route
-        path="/login"
-        render={props => {
-          if (isAuthenticated) {
-            props.history.goBack()
-          } else {
-            return <LogIn />
-          }
-        }
-        }
-      />
+      <CustomRoute path="/login" component={<LogIn />} authed={isAuthenticated} />
+      <CustomRoute path="/signup" component={<SignUp />} authed={isAuthenticated} />
       <Route component={NotFound} />
     </Switch>
   )
