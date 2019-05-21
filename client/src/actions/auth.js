b58610ed62ff4ba68fc75d0795f2aa8613e73fc8
BeijiYang <langyuyang@outlook.com>
2019-05-21 21:50:08 +0200

    refactor: auth action

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -2,9 +2,13 @@ import axios from 'axios'
 import { showNotificaton } from './notification'
 import { LOGIN, LOGOUT } from '../actions/types'
 
-const url = 'http://localhost:3001/login'
+const serverUrl = 'http://localhost:3001'
+const loginUrl = `${serverUrl}/login`
+const signupUrl = `${serverUrl}/signup`
 
-const loginSuccess = (msg) => msg === 'LOGIN_SUCCESS'
+const loginSuccess = 'LOGIN_SUCCESS'
+const signupSuccess = 'SIGNUP_SUCCESS'
+const checkIf = successMsg => msg => successMsg === msg
 
 const logIn = data => ({
   type: LOGIN,
@@ -17,10 +21,10 @@ const logOut = () => ({
 
 const handleError = () => { }
 
-export const login = (username, password) => async dispatch => {
-  const resData = await axios.post(url, { username, password }).then(res => res.data).catch(err => err.response)
+const login = (username, password) => async dispatch => {
+  const resData = await axios.post(loginUrl, { username, password }).then(res => res.data).catch(err => err.response)
   const { msg, ...data } = resData
-  if (loginSuccess(msg)) {
+  if (checkIf(loginSuccess)(msg)) {
     const actions = [
       logIn(data),
       showNotificaton('login success')
@@ -31,10 +35,26 @@ export const login = (username, password) => async dispatch => {
   }
 }
 
-export const logout = () => async dispatch => {
+const signup = (username, password) => async dispatch => {
+  const resData = await axios.post(signupUrl, { username, password }).then(res => res.data).catch(err => err.response)
+  const { msg, ...data } = resData
+  if (checkIf(signupSuccess)(msg)) {
+    const actions = [
+      logIn(data),
+      showNotificaton('signup success')
+    ]
+    dispatch(actions)
+  } else {
+    handleError()
+  }
+}
+
+const logout = () => async dispatch => {
   const actions = [
     logOut(),
     showNotificaton('logout success')
   ]
   dispatch(actions)
 }
+
+export { login, signup, logout }
