12513ae945aa5d639906601a97518cd21405ffa6
BeijiYang <langyuyang@outlook.com>
2019-05-22 14:55:25 +0200

    refactor: show paged list on web

--- client/src/actions/cardsList.js
+++ client/src/actions/cardsList.js
@@ -1,19 +1,63 @@
-import { FETCH_CARDS, UPDATE_SCROLL_TOP } from './types'
+import { UPDATE_CARDS, UPDATE_SCROLL_TOP, UPDATE_CUR_PAGE_INDEX, UPDATE_CUR_PAGE_FOR_MOBILE, UPDATE_CUR_PAGE_FOR_WEB } from './types'
 import axios from 'axios'
 
 const CARDS_NUM_PER_PAGE = 12
 const url = 'http://localhost:3001/companies'
 
-const fetchCards = (pageIndex = 0) => async (dispatch, getState) => {
-  const { cardsList: { cards } } = getState()
-  const savedPageNum = cards.filter(Boolean).length / CARDS_NUM_PER_PAGE
-  if (pageIndex < savedPageNum) return
+const fetcher = async pageIndex =>
+  await axios.post(url, { pageIndex, cardsNumPerPage: CARDS_NUM_PER_PAGE })
+    .then(res => res.data)
+    .catch(err => err)
 
-  const data = await axios.post(url, { pageIndex, cardsNumPerPage: CARDS_NUM_PER_PAGE }).then(res => res.data)
-  dispatch({
-    type: FETCH_CARDS,
-    payload: data
-  })
+const updateCurPageForMobile = () => ({
+  type: UPDATE_CUR_PAGE_FOR_MOBILE,
+})
+
+const updateCurPageForWeb = (data) => ({
+  type: UPDATE_CUR_PAGE_FOR_WEB,
+  payload: data
+})
+
+const updateCurPageIndex = (newIndex) => ({
+  type: UPDATE_CUR_PAGE_INDEX,
+  payload: newIndex
+})
+
+const updateCards = data => ({
+  type: UPDATE_CARDS,
+  payload: data
+})
+
+const nextPageForMobile = (pageIndex = 0) => async dispatch => {
+  const data = await fetcher(pageIndex)
+  const actions = [
+    updateCards(data),
+    updateCurPageIndex(pageIndex),
+    updateCurPageForMobile()
+  ]
+  dispatch(actions)
+}
+
+const calculateFromCards = (pageIndex, savedCards) => {
+  const start = pageIndex * CARDS_NUM_PER_PAGE
+  const end = (pageIndex + 1) * CARDS_NUM_PER_PAGE
+  const companies = savedCards.slice(start, end)
+  return { companies }
+}
+
+const nextPageForWeb = (pageIndex = 0) => async (dispatch, getState) => {
+  const { cardsList: { cards } } = getState()
+  const savedPageNum = cards.length / CARDS_NUM_PER_PAGE
+  const needToFetch = pageIndex === savedPageNum
+
+  const data = needToFetch ? await fetcher(pageIndex) : calculateFromCards(pageIndex, cards)
+  const actions = [
+    updateCurPageIndex(pageIndex),
+    updateCurPageForWeb(data)
+  ]
+  if (needToFetch) actions.unshift(updateCards(data))
+
+  dispatch(actions)
 }
 
 const updateScrollTop = newScrollTop => ({
@@ -21,4 +65,4 @@ const updateScrollTop = newScrollTop => ({
   payload: newScrollTop
 })
 
-export { fetchCards, updateScrollTop }
+export { nextPageForMobile, nextPageForWeb, updateScrollTop }
--- client/src/reducers/cardsList.js
+++ client/src/reducers/cardsList.js
@@ -1,9 +1,10 @@
-import { FETCH_CARDS, UPDATE_SCROLL_TOP } from '../actions/types'
+import { UPDATE_CARDS, UPDATE_CUR_PAGE_FOR_MOBILE, UPDATE_CUR_PAGE_FOR_WEB, UPDATE_CUR_PAGE_INDEX, UPDATE_SCROLL_TOP } from '../actions/types'
 
 const CARDS_NUM_PER_PAGE = 12
 
 const initialState = {
-  cards: Array(CARDS_NUM_PER_PAGE).fill(null),
+  cards: [],
+  cardsOnCurPage: Array(CARDS_NUM_PER_PAGE).fill(null),
   curPageIndex: 0,
   totalPageNum: 0,
   scrollTop: 0,
@@ -11,13 +12,31 @@ const initialState = {
 
 const homeReducer = (state = initialState, { type, payload }) => {
   switch (type) {
-    case FETCH_CARDS:
+    case UPDATE_CARDS:
       const { companies, totalPageNum } = payload
       return {
         ...state,
         cards: [...state.cards.filter(Boolean), ...companies],
-        curPageIndex: ++state.curPageIndex,
         totalPageNum
+      }
+
+    case UPDATE_CUR_PAGE_INDEX:
+      return {
+        ...state,
+        curPageIndex: payload,
+      }
+
+    case UPDATE_CUR_PAGE_FOR_MOBILE:
+      return {
+        ...state,
+        cardsOnCurPage: state.cards
+      }
+
+    case UPDATE_CUR_PAGE_FOR_WEB:
+      const { companies: companiesToShow } = payload
+      return {
+        ...state,
+        cardsOnCurPage: companiesToShow
       }
 
     case UPDATE_SCROLL_TOP:
