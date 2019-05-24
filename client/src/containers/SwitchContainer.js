38ba7d13687de365ca887dc098b32f0a3e22ab9a
BeijiYang <langyuyang@outlook.com>
2019-05-24 23:22:49 +0200

    add: commit about the interface of the component

--- client/src/containers/SwitchContainer.js
+++ client/src/containers/SwitchContainer.js
@@ -2,6 +2,9 @@ import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import Switch from '../components/Switch'
 
+/**
+ * interface: this.props.onSwitch function
+ */
 class SwitchContainer extends Component {
 
   static propTypes = {
